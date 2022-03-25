ca9f13b48536b0c5730aef42a52ffb3b920e7874
M3NY <menyvarela23@gmail.com>
2022-03-25 17:19:08 -0600

    Pruebas unitarias del lamdas

---
+++ LamdasChallenge/src/TestCalculator.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class TestCalculator {
+    
+    @Test
+    public void add() {
+        assertEquals(Calculator.add(2, 45), 2 + 45);
+    }
+
+    @Test
+    public void sub() {
+        assertEquals(Calculator.sub(2, 45), 2 - 45);
+    }
+
+    @Test
+    public void isOdd() {
+        assertTrue(Calculator.isOdd(5) == false);
+    }
+
+    @Test
+    public void isPrime() {
+        assertTrue(Calculator.isPrime(3) == true);
+    }
+
+    @Test
+    public void getFactorial() {
+        assertEquals(Calculator.getFactorial(3), 6);
+    }
+}
